466f27af6ee50f42770083b5cc3c186efd794675
Ramiro Brandão <ramirobrandao@outlook.com>
2025-02-08 23:09:31 -0300

    test: Adicionando arquivo test.cy.js

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,14 @@
+//funcionalidade
+describe('Login', () => {
+
+    //cenário de sucesso
+    it.only('Teste depuração env secrects - Ci Actions', () => {
+
+        const username = Cypress.env('username')
+        const password = Cypress.env('password')
+
+        cy.log(`Username: ${username}`);
+        cy.log(`Password: ${password}`);
+
+    })
+})
